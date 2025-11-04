9b191cb77ae35100ce4a8baf2a07c69b8cd35814
Desri Nur Fadilah <desri@example.com>
2025-11-04 16:41:09 +0700

    Menambahkan koneksi.php

---
+++ koneksi.php
@@ -0,0 +1,12 @@
+echo "<?php
+\$servername = 'localhost';
+\$username = 'root';
+\$password = '';
+\$database = '';
+\$conn = mysqli_connect(\$servername, \$username, \$password, \$database);
+if (!\$conn) {
+    die('Koneksi gagal: ' . mysqli_connect_error());
+} else {
+    echo 'Koneksi ke database berhasil!';
+}
+?>" > koneksi.php
